b4859842bda89e441aa1d61dbcccd24cdeca4578
jmartin <jmartin@zikzakmedia.com>
2015-03-27 12:53:02 +0100

    Add test view to unittest

--- tests/test_sale_payment_type_cost.py
+++ tests/test_sale_payment_type_cost.py
@@ -1,7 +1,7 @@
 # This file is part of sale_payment_type_cost module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_depends
+from trytond.tests.test_tryton import test_view, test_depends
 import trytond.tests.test_tryton
 import unittest
 
@@ -11,6 +11,10 @@ class TestCase(unittest.TestCase):
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('sale_payment_type_cost')
+
+    def test0005views(self):
+        'Test views'
+        test_view('sale_payment_type_cost')
 
     def test0006depends(self):
         'Test depends'
