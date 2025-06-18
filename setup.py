57b9e4d778fc03231332d0a0fe74ee05519d93d1
Raimon Esteve <raimonesteve@gmail.com>
2025-06-18 13:48:00 +0200

    FIX Removing deprecated args from setup

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ from configparser import ConfigParser
 MODULE = 'sale_payment_type_cost'
 PREFIX = 'nantic'
 MODULE2PREFIX = {
-    'sale_payment_type',
-    'account_payment_type_cost',
+    'sale_payment_type': 'nantic',
+    'account_payment_type_cost': 'nantic',
 }
 
 
@@ -80,7 +80,7 @@ setup(name='%s_%s' % (PREFIX, MODULE),
         ],
     package_data={
         'trytond.modules.%s' % MODULE: (info.get('xml', [])
-            + ['tryton.cfg', 'locale/*.po', 'tests/*.rst']),
+            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', 'tests/*.rst']),
         },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
